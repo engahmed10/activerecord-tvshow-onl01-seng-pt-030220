6a9595550dd2d4c10cb9a3788346b25ac1bf425e
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-09 02:16:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -2,11 +2,9 @@ class  CreateShows < ActiveRecord::Migration[5.2]
   def change
 
     create_table :shows do |t|
-     t.string :name
-     t.string :network
-     t.string :rating
-     
-
+       t.string :name
+        t.string :network
+        t.integer :rating
     end
 
   end
